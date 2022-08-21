6ada9b5f7de2659d3dba019691af18e46b0e2e1e
adithya89780 <adithyasachin123@gmail.com>
2022-08-21 18:32:51 +0530

    Added: Create Contacts

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -3,5 +3,22 @@ package com.bridgelabz;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book ");
+        Contacts contact = new Contacts();
+        contact.setFirstName("Adithya");
+        System.out.println(contact.getFirstName());
+        contact.setLastName("veldandi");
+        System.out.println(contact.getLastName());
+        contact.setAddress("12-10-264/2");
+        System.out.println(contact.getAddress());
+        contact.setCity("hyderabad");
+        System.out.println(contact.getCity());
+        contact.setState("Telangana");
+        System.out.println(contact.getState());
+        contact.setZip(50061);
+        System.out.println(contact.getZip());
+        contact.setPhoneNumber(1234567890);
+        System.out.println(contact.getPhoneNumber());
+        contact.setEmail("123@gmail.com");
+        System.out.println(contact.getEmail());
     }
 }
