66b0c6910fc3025f760ab4ad8d6841749fa4cc19
adithya89780 <adithyasachin123@gmail.com>
2022-08-23 10:29:01 +0530

    Added: Edit contacts

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -22,6 +22,35 @@ public class AddressBook {
         System.out.println("Enter phoneNumber :");
         contact.setPhoneNumber(scan.nextLong());
     }
+    void editContact() {
+        System.out.println("Enter first name of person to edit");
+        String firstName = scan.next();
+        if (firstName.equals(contact.getFirstName())) {
+            System.out.println("Enter the details of person");
+            System.out.println("Enter first name");
+            contact.setFirstName(scan.next());
+
+            System.out.println("Enter lastName");
+            contact.setLastName(scan.next());
+
+            System.out.println("Enter city");
+            contact.setCity(scan.next());
+
+            System.out.println("Enter state");
+            contact.setState(scan.next());
+
+            System.out.println("Enter email");
+            contact.setEmail(scan.next());
+
+            System.out.println("Enter zip");
+            contact.setZip(scan.nextInt());
+
+            System.out.println("Enter phoneNumber");
+            contact.setPhoneNumber(scan.nextLong());
+        } else {
+            System.out.println("person not found");
+        }
+    }
     Contacts display() {
         return contact;
     }
--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -12,11 +12,9 @@ public class Contacts {
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
@@ -24,35 +22,27 @@ public class Contacts {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public String getCity() {
         return city;
     }
-
     public void setCity(String city) {
         this.city = city;
     }
-
     public String getState() {
         return state;
     }
-
     public void setState(String state) {
         this.state = state;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
@@ -68,7 +58,6 @@ public class Contacts {
     public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
     public String toString() {
         return "Contacts{" +
                 "firstName='" + firstName + '\'' +
