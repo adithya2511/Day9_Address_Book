9adac5952bc6c4a5c0b8bc5ad338dbe6d88a3e36
adithya89780 <adithyasachin123@gmail.com>
2022-08-23 10:09:14 +0530

    Added: New contacts

--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -10,39 +10,49 @@ public class Contacts {
     private long phoneNumber;
     private String email;
     public String getFirstName() {
-        System.out.println();
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public String getState() {
         return state;
     }
+
     public void setState(String state) {
         this.state = state;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -57,5 +67,18 @@ public class Contacts {
     }
     public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String toString() {
+        return "Contacts{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", email='" + email + '\'' +
+                ", zip=" + zip +
+                ", phoneNumber=" + phoneNumber +
+                '}';
     }
 }
